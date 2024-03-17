cbf5c889b72536fb66e68f71347b8a535a89f7d1
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2024-03-17 16:32:14 -0400

    Create calculator operation functions

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const operate = (operator, a, b) => {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+    }
+
+    return isFinite(result) ? result.toString(10) : "ERR"
+};
