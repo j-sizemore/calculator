61d94cf4515e640d23427c6be51e0ef7e06119de
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2024-03-21 09:11:40 -0400

    Add basic calculator functionality

--- script.js
+++ script.js
@@ -21,3 +21,32 @@ const operate = (operator, a, b) => {
 
     return isFinite(result) ? result.toString(10) : "ERR"
 };
+
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        let currentDisplay = display.textContent;
+        let newDisplay;
+        let entry = button.textContent;
+
+        if (entry === "CE") {
+            newDisplay = ""
+        } else if (entry === "←") { 
+            newDisplay = currentDisplay.slice(0, currentDisplay.length - 1);
+        } else if (entry.search(/[\d.]/) > -1) { // number or decimal
+            newDisplay = currentDisplay + entry;
+        } else if (entry.search(/[+\-*/]/) > -1) { // operator
+            newDisplay = currentDisplay + " " + entry + " ";
+        } else if (entry === "=") {
+            let expression = currentDisplay.split(" ");
+            newDisplay = operate(expression[1], +expression[0], +expression[2]);
+        } else {
+            console.error("Unexpected button value: " + entry);
+            newDisplay = "ERR";
+        }
+
+        display.textContent = newDisplay;
+
+    });
+});
